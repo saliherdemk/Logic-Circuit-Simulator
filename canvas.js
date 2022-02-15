59d60f3620ce3e471e0e977266afccc644e18500
Salih Erdem Kaymak <20060365@stu.omu.edu.tr>
2022-02-15 21:52:06 +0300

    Second Commit

--- canvas.js
+++ canvas.js
@@ -1,34 +1,31 @@
 function setup() {
     createCanvas(windowWidth, windowHeight - 100);
 }
-  
+
 function draw() {
     background(220);
     for (let i = 0; i < currentInputs.length; i++) {
-        currentInputs[i].show()
-        currentInputs[i].over()
-        currentInputs[i].update()
-        
+        currentInputs[i].draw()
+
     }
 
     for (let i = 0; i < currentAndGates.length; i++) {
-        currentAndGates[i].show()
-        currentAndGates[i].over()
-        currentAndGates[i].update()
+        currentAndGates[i].draw()
 
     }
 
     for (let i = 0; i < currentOutputs.length; i++) {
-        currentOutputs[i].show()
-        currentOutputs[i].over()
-        currentOutputs[i].update()
+        currentOutputs[i].draw()
+
 
     }
 }
 
-function mousePressed(){
+function mousePressed() {
     for (let i = 0; i < currentInputs.length; i++) {
         currentInputs[i].pressed()
+        currentInputs[i].active()
+
     }
 
     for (let i = 0; i < currentAndGates.length; i++) {
@@ -37,6 +34,8 @@ function mousePressed(){
 
     for (let i = 0; i < currentOutputs.length; i++) {
         currentOutputs[i].pressed()
+        currentOutputs[i].active()
+
     }
 }
 
@@ -51,17 +50,16 @@ function mouseReleased() {
     for (let i = 0; i < currentOutputs.length; i++) {
         currentOutputs[i].released()
     }
-  }
+}
 
 
-
-function doubleClicked(){
+function doubleClicked() {
     for (let i = 0; i < currentInputs.length; i++) {
         currentInputs[i].changeValue()
     }
 }
 
-function dist(x1,y1,x2,y2){
-    return ((x2 -x1) ** 2) + ((y2 - y1) ** 2)
+function dist(x1, y1, x2, y2) {
+    return ((x2 - x1) ** 2) + ((y2 - y1) ** 2)
 
 }
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var currentInputs = []
 var currentOutputs = []
 var currentAndGates = []
+var currentLines = []
 
 function generateInput() {
-    let input = new Input(0,50,200);
+    let input = new Input(0, 50, 200);
     currentInputs.push(input)
 }
 
-function generateOutput(){
-    let output = new Output(0,50,200);
+function generateOutput() {
+    let output = new Output(0, 50, 200);
     currentOutputs.push(output)
 
 }
 
-function generateAndGate(){
-    let andGate = new AndGate(1,1,50,200);
+function generateAndGate() {
+    let andGate = new AndGate(1, 1, 50, 200);
     currentAndGates.push(andGate)
 
 }
